d4bab0ffa2acc4822ef7803d1599c6eec576d01a
Aleksei Tiurin <alexeytyurin74@gmail.com>
2023-11-01 00:01:32 +0200

    Update build-docker-runner.sh

--- scripts/build-docker-runner.sh
+++ scripts/build-docker-runner.sh
@@ -3,4 +3,10 @@ echo "building farm-cli-client $APP_VERSION"
 ./gradlew clean :farm-cli-client:distZip
 cp farm-cli-client/build/app/distributions/farm-cli-client-$APP_VERSION.zip farm-docker-runner/docker/rootfs/$APPS_DIR/farm-cli-client.zip
 cd farm-docker-runner/docker
-docker build --build-arg APP_VERSION=$APP_VERSION -t android-runner .
+if [ -z "$1" ]; then
+  IMAGE_NAME="android-runner"
+else
+  IMAGE_NAME="$1"
+fi
+echo "Build docker image"
+docker build --build-arg APP_VERSION=$APP_VERSION -t IMAGE_NAME .
