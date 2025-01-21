006b218b9a1abe88841724de1762ef61707ad439
Aleksei Tiurin <alexeytyurin74@gmail.com>
2025-01-21 16:20:42 +0200

    delete device fix

--- farm-docker-runner/docker/rootfs/docker/build.d/30-android-sdk.sh
+++ farm-docker-runner/docker/rootfs/docker/build.d/30-android-sdk.sh
@@ -10,7 +10,3 @@ rm -rf platform-tools.zip
 wget -nv -O tools.zip https://dl.google.com/android/repository/commandlinetools-linux-7302050_latest.zip && \
 unzip -q tools.zip -d ${ANDROID_HOME}/cmdline-tools && \
 rm -rf tools.zip
-
-wget -nv -O emulator.zip  https://dl.google.com/android/repository/emulator-linux_x64-7324830.zip && \
-unzip -q emulator.zip -d ${ANDROID_HOME} && \
-rm -rf emulator.zip
